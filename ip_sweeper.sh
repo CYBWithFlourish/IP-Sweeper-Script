2c733f78de9212890dc06ffe8f9055f5074c5ac1
Flourish <project.samclak@gmail.com>
2024-01-26 12:38:02 +0100

    Updated

--- ip_sweeper.sh
+++ ip_sweeper.sh
@@ -22,11 +22,27 @@ spinner() {
 }
 
 echo -n "Processing ping sweep... "
+
+#To store output on a text file, create an array to store responsive IP addresses
+#responsive_ips=()
+
 {
+# Loop through IP addresses    
   for ip in $(seq 1 254); do
     ping -c 1 "$1.$ip" | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    #if ping -c 1 $1.$ip | grep "64 bytes" > /dev/null; then
+    #    echo "$1.$ip responded to ping"
+    #    responsive_ips+=("$1.$ip")
+    #fi 
   done
 } &
+
+# Save reesponsive IPs to a text file
+# if [ ${#responded_ips[@]} -eq 0 ]; then
+#   echo "No devices found." > output.txt
+# else
+#   echo "${responsive_ips[@]}" > output.txt
+# fi
 
 spinner $!
 
